5cb4695a734e878c8014e8c64ab7389b034904bd
emilykuowen <emily.kuowen@gmail.com>
2023-12-07 11:55:57 -0800

    added training and validation loss plots

--- deepafx_st/read_checkpoint.py
+++ deepafx_st/read_checkpoint.py
@@ -1,29 +1,51 @@
 import torch
 from cadenza_system import CadenzaSystem
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Specify the path to the checkpoint file
-checkpoint_path = 'cadenza_model_checkpoint_epoch0.pth'
+training_loss_array = []
+validation_loss_array = []
 
-# Load the checkpoint
-checkpoint = torch.load(checkpoint_path)
+for i in range(15):
+    # Specify the path to the checkpoint file
+    checkpoint_path = 'checkpoints/cadenza_model_checkpoint_epoch' + str(i) + '.pth'
 
-# Access the model parameters
-model_state_dict = checkpoint['model_state_dict']
+    # Load the checkpoint
+    checkpoint = torch.load(checkpoint_path)
 
-# Access additional information if available
-# For example, epoch, loss, etc.
-epoch = checkpoint['epoch']
-training_loss = checkpoint['training_losses']
-validation_loss = checkpoint['validation_losses']
+    # Access additional information if available
+    # For example, epoch, loss, etc.
+    epoch = checkpoint['epoch']
+    training_loss_array += checkpoint['training_losses']
+    validation_loss_array += checkpoint['validation_losses']
 
-print("epoch", epoch)
-print("training losses", training_loss)
-print("validation losses", validation_loss)
+    print("epoch", epoch)
+    # print("training losses", training_loss)
+    # print("validation losses", validation_loss)
+
+print(len(training_loss_array))
+
+plt.plot(training_loss_array)
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title('Training Loss')
+plt.savefig('training_loss.png')
+plt.show()
+
+plt.plot(validation_loss_array)
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title('Validation Loss')
+plt.savefig('validation_loss.png')
+plt.show()
 
 # Now, you can use the loaded information as needed
 # For example, to load the model and optimizer state:
 # Create your model instance
-model = CadenzaSystem()
+# model = CadenzaSystem()
 
-# Load the model state
-model.load_state_dict(model_state_dict)
+# Access the model parameters
+# model_state_dict = checkpoint['model_state_dict']
+
+# # Load the model state
+# model.load_state_dict(model_state_dict)
